ceea159cd147c605490ec53dc1446b5e94d6271b
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-12 21:17:05 -0700

    Update forms, tracks#create

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,11 +4,20 @@ class TracksController < ApplicationController
   end
 
   def new
-
+    @album = Album.find_by_id(params[:album_id])
+    @track = Track.new
   end
 
   def create
 
+    @track = Track.new(track_params)
+    if @track.save
+      flash.notice = "#{@track.name} successfully added to #{@track.album.name}"
+      redirect_to album_url(@track.album)
+    else
+      flash.now[:errors] = @track.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -26,5 +35,8 @@ class TracksController < ApplicationController
 
   def destroy
   end
-  
+
+  def track_params
+    params.require(:track).permit(:name, :album_id, :track_type, :lyrics)
+  end
 end
